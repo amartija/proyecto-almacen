5c86ed698bd31e045ec45ff987d8102498fe0db3
Amaia Martija <amaia.martija@hotmail.com>
2017-05-16 18:10:18 +0200

    Añadida etiqueta CrossOrigin en los archivos de servicios REST, restController

--- gestion-almacen-web/src/main/java/com/ipartek/formacion/api/webserver/prenda/PrendaRestController.java
+++ gestion-almacen-web/src/main/java/com/ipartek/formacion/api/webserver/prenda/PrendaRestController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,6 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ipartek.formacion.persistence.Prenda;
 import com.ipartek.formacion.service.interfaces.PrendaService;
 
+@CrossOrigin(origins = "*", maxAge = 3600, methods = { RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT,
+		RequestMethod.DELETE })
 @RestController
 @RequestMapping("/api/prendas")
 public class PrendaRestController {
